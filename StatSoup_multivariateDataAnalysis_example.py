e28859137163b59c8a78bf07dd2f57b0055675e7
ptruong <ptruong@kth.se>
2018-04-18 14:22:34 +0200

    updated example file added wine.csv

--- StatSoup_multivariateDataAnalysis_example.py
+++ StatSoup_multivariateDataAnalysis_example.py
@@ -141,8 +141,8 @@ def calcWithinGroupsVariance(variable, groupvariable): #own formula for understa
         # Summation procedure in within-group variance formula
         num_total = num_total + num_i
         denom_total = denom_total + denom_i
-    S_w = num_total / (denom_total - num_unique_groups)
-    return S_w
+    V_w = num_total / (denom_total - num_unique_groups)
+    return V_w
 
 ## Between-Groups Variance for a variable
 def calcBetweenGroupsVariance(variable, groupvariable):
@@ -168,4 +168,128 @@ def calcBetweenGroupsVariance(variable, groupvariable):
     Vb = numtotal / (numlevels - 1)
     return(Vb)
     
+#variable = X.V2
+#groupvariable = Y
+def calcBetweenGroupsVariance(variable, groupvariable): #own formula for understanding
     
+    # find out how many values the group variable can take
+    unique_groups = groupvariable.unique()
+    num_unique_groups = len(unique_groups)
+    
+    #calculate the overall grand mean
+    grand_mean = np.mean(variable)
+    
+    # the the mean and standard deviation for each group
+    num_total = 0
+    denom_total = 0
+    for i in unique_groups:
+        group_i = variable[groupvariable == i]
+        len_group_i = len(group_i)
+        # get the mean and standard deviation for group i
+        mean_i = np.mean(group_i)
+        std_i = np.std(group_i)
+        # Between-group variance formula
+        num_i = (len_group_i) * ((mean_i - grand_mean)**2)
+        denom_i = (len_group_i)
+        # Summation procedure in between-group variance formula
+        num_total = num_total + num_i
+        denom_total = denom_total + denom_i
+    # valvulate the between-groups variance
+    V_b = num_total / (num_unique_groups - 1) 
+    return(V_b)
+
+# Calculate seperation achieved by a variable - Seperation achieved by V2
+calcBetweenGroupsVariance(X.V2, y) / calcWithinGroupsVariance(X.V2, y)
+
+# Calculate Seperation acieved by all variables in a multivariate dataset
+def calcSeparations(variables, groupvariable):
+    # calculate the separation for each variable
+    for variablename in variables:
+        variablei = variables[variablename]
+        Vw = calcWithinGroupsVariance(variablei, groupvariable)
+        Vb = calcBetweenGroupsVariance(variablei, groupvariable)
+        sep = Vb/Vw
+        print("variable", variablename, "Vw=", Vw, "Vb=", Vb, "separation=", sep)
+
+#variables = X
+#groupvariable = y
+
+def calcSeparations(variables, groupvariable):
+    # Calculate the separation for each variable
+    for i in variables:
+        variable_i = variables[i]
+        V_w = calcWithinGroupsVariance(variable_i, groupvariable) 
+        V_b = calcBetweenGroupsVariance(variable_i, groupvariable)
+        sep = V_b/V_w
+        print( "variable", i, "V_w=", V_w, "V_b=", V_b, "separation=", sep)
+    
+
+## Between-groups Covariance and Within-groups Covariance for Two Variables
+
+def calcWithinGroupsCovariance(variable1, variable2, groupvariable):
+    levels = sorted(set(groupvariable))
+    numlevels = len(levels)
+    Covw = 0.0
+    # get the covariance of variable 1 and variable 2 for each group:
+    for leveli in levels:
+        levelidata1 = variable1[groupvariable==leveli]
+        levelidata2 = variable2[groupvariable==leveli]
+        mean1 = np.mean(levelidata1)
+        mean2 = np.mean(levelidata2)
+        levelilength = len(levelidata1)
+        # get the covariance for this group:
+        term1 = 0.0
+        for levelidata1j, levelidata2j in zip(levelidata1, levelidata2):
+            term1 += (levelidata1j - mean1)*(levelidata2j - mean2)
+        Cov_groupi = term1 # covariance for this group
+        Covw += Cov_groupi
+    totallength = len(variable1)
+    Covw /= totallength - numlevels
+    return Covw
+
+variable = X.V8
+variable = X.V11
+groupvariable = y
+
+
+# find out how many values the group variables can take
+unique_groups = groupvariable.unique
+num_unique_groups = len(unique_groups)
+CovW = 0.0
+
+# get the covarance of variable 1 and variable 2 for each groups
+for i in unique_groups:
+    group_i_var1 = variable1
+    group_i_var2 = variable2
+    mean_var1 = np.mean(group_i_var1)
+    mean_var2 = np.mean(group_i_var2)
+    
+
+
+def calcBetweenGroupsVariance(variable, groupvariable): #own formula for understanding
+    
+    # find out how many values the group variable can take
+    unique_groups = groupvariable.unique()
+    num_unique_groups = len(unique_groups)
+    
+    #calculate the overall grand mean
+    grand_mean = np.mean(variable)
+    
+    # the the mean and standard deviation for each group
+    num_total = 0
+    denom_total = 0
+    for i in unique_groups:
+        group_i = variable[groupvariable == i]
+        len_group_i = len(group_i)
+        # get the mean and standard deviation for group i
+        mean_i = np.mean(group_i)
+        std_i = np.std(group_i)
+        # Between-group variance formula
+        num_i = (len_group_i) * ((mean_i - grand_mean)**2)
+        denom_i = (len_group_i)
+        # Summation procedure in between-group variance formula
+        num_total = num_total + num_i
+        denom_total = denom_total + denom_i
+    # valvulate the between-groups variance
+    V_b = num_total / (num_unique_groups - 1) 
+    return(V_b)
